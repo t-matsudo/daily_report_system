1c9212637e04cf294d75c269aff2660db40a7819
tsuyoshi.matsudo <matsudot@md-s.co.jp>
2023-12-13 15:36:15 +0900

    従業員編集画面の表示まで作成

--- src/main/java/actions/EmployeeAction.java
+++ src/main/java/actions/EmployeeAction.java
@@ -111,6 +111,11 @@ public class EmployeeAction extends ActionBase {
         }
     }
 
+    /**
+     * 従業員データを取得し、編集画面に表示
+     * @throws ServletException
+     * @throws IOException
+     */
     public void show() throws ServletException, IOException{
         EmployeeView ev = service.findOne(toNumber(getRequestParam(AttributeConst.EMP_ID)));
 
@@ -124,5 +129,18 @@ public class EmployeeAction extends ActionBase {
         putRequestScope(AttributeConst.EMPLOYEE, ev);
 
         forward(ForwardConst.FW_EMP_SHOW);
+    }
+
+    public void edit() throws ServletException, IOException{
+        EmployeeView ev = service.findOne(toNumber(getRequestParam(AttributeConst.EMP_ID)));
+
+        if(ev == null || ev.getDeleteFlag() == AttributeConst.DEL_FLAG_TRUE.getIntegerValue()) {
+            forward(ForwardConst.FW_ERR_UNKNOWN);
+            return;
+        }
+        putRequestScope(AttributeConst.TOKEN,getTokenId());
+        putRequestScope(AttributeConst.EMPLOYEE, ev);
+
+        forward(ForwardConst.FW_EMP_EDIT);
     }
 }
