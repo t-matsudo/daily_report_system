7ca8f0f992ca1d929decca7215aa5646b8cabafd
tsuyoshi.matsudo <matsudot@md-s.co.jp>
2023-12-18 11:57:14 +0900

    日報showアクションの作成

--- src/main/java/actions/ReportAction.java
+++ src/main/java/actions/ReportAction.java
@@ -114,4 +114,22 @@ public class ReportAction extends ActionBase {
 
         }
     }
+
+    /**
+     * 詳細画面を表示する
+     * @throws ServletException
+     * @throws IOException
+     */
+    public void show() throws ServletException, IOException{
+        //IDを条件に日報を1件取得
+        ReportView rv = service.findOne(toNumber(getRequestParam(AttributeConst.REP_ID)));
+        if(rv == null) {
+            //該当データがない場合、エラー
+            forward(ForwardConst.FW_ERR_UNKNOWN);
+        }else {
+            putRequestScope(AttributeConst.REPORT, rv);
+
+            forward(ForwardConst.FW_REP_SHOW);
+        }
+    }
 }
